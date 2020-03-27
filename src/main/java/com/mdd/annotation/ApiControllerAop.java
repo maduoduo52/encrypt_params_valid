a98e8f3abe8e4d2ed64067a5607584b5e5d6be47
maduoduo <ml525282627@163.com>
2020-03-27 10:01:38 +0800

    [CHG]:增加处理实体对象包含List对象的参数校验

--- src/main/java/com/mdd/annotation/ApiControllerAop.java
+++ src/main/java/com/mdd/annotation/ApiControllerAop.java
@@ -1,6 +1,7 @@
 package com.mdd.annotation;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.mdd.util.AESCodeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.FieldUtils;
@@ -14,6 +15,8 @@ import org.springframework.util.CollectionUtils;
 
 import javax.validation.constraints.NotNull;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -62,24 +65,47 @@ public class ApiControllerAop {
         //获取需要校验的类信息
         Class claz = reqAop.clazz();
 
-        //获取需要校验类的全部属性信息
-        Field[] fields = FieldUtils.getAllFields(claz);
+        validField(paramMap, validList, claz);
+    }
 
+    private void validField(Map<String, Object> paramMap, List<String> validList, Class clazz) {
+        //获取需要校验类的全部属性信息
+        Field[] fields = FieldUtils.getAllFields(clazz);
         for (Field field : fields) {
+            //处理List类型属性
+            if (List.class.isAssignableFrom(field.getType())) {
+                Type t = field.getGenericType();
+                if (t instanceof ParameterizedType) {
+                    ParameterizedType pt = (ParameterizedType) t;
+                    //得到对象list中实例的类型
+                    Class clz = (Class) pt.getActualTypeArguments()[0];
+                    Object o = paramMap.get(field.getName());
+                    if (o instanceof JSONArray) {
+                        for (Object o1 : ((JSONArray)o)) {
+                            //获取list对象的属性
+                            Map<String, Object> listParamMap = JSON.parseObject(JSON.toJSONString(o1));
+                            if (!listParamMap.isEmpty()) {
+                                //递归校验List参数信息
+                                validField(listParamMap, validList, clz);
+                            }
+                        }
+                    }
+                }
+            }
             //获取参数值
             Object o = paramMap.get(field.getName());
             //获取NotBlank注解
             NotBlank blank = field.getAnnotation(NotBlank.class);
             if (blank != null) {
                 if (Objects.isNull(o)) {
-                    validList.add(blank.message());
+                    validList.add(clazz.getSimpleName() + "." + blank.message());
                 }
             }
             //获取NotNull注解
             NotNull notNull = field.getAnnotation(NotNull.class);
             if (notNull != null) {
                 if (Objects.isNull(o)) {
-                    validList.add(notNull.message());
+                    validList.add(clazz.getSimpleName() + "." + notNull.message());
                 }
             }
         }
